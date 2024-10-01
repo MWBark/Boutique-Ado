768c558efa20ebe21acc8e192cf5e2a27610adbf
Michael Baker <michael.w.barker@hotmail.co.uk>
2024-10-01 18:03:15 +0000

    Updated allauth templates and tested profiles

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -103,7 +103,7 @@ AUTHENICATION_BACKENDS = (
 
 SITE_ID = 1
 
-EMAIL_BACKENDS = 'django.mail.core.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
